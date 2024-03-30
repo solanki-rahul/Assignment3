3e42cddc92e2d3309816444c187fb4538681f464
solanki-rahul <1rahul32003@gmail.com>
2024-03-29 20:58:57 -0400

    Database Schema File

---
+++ schema.sql
@@ -0,0 +1,93 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Product] (
+    [Id] int NOT NULL IDENTITY,
+    [Description] nvarchar(max) NULL,
+    [Image] nvarchar(max) NULL,
+    [Pricing] decimal(18,2) NOT NULL,
+    [ShippingCost] decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_Product] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [User] (
+    [Id] int NOT NULL IDENTITY,
+    [Email] nvarchar(max) NULL,
+    [Password] nvarchar(max) NULL,
+    [Username] nvarchar(max) NULL,
+    [PurchaseHistory] nvarchar(max) NULL,
+    [ShippingAddress] nvarchar(max) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Cart] (
+    [Id] int NOT NULL IDENTITY,
+    [ProductId] int NOT NULL,
+    [Quantity] int NOT NULL,
+    [UserId] int NOT NULL,
+    CONSTRAINT [PK_Cart] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Cart_Product_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Product] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Cart_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Comment] (
+    [Id] int NOT NULL IDENTITY,
+    [ProductId] int NOT NULL,
+    [UserId] int NOT NULL,
+    [Rating] int NOT NULL,
+    [Image] nvarchar(max) NULL,
+    [Text] nvarchar(max) NULL,
+    CONSTRAINT [PK_Comment] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Comment_Product_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Product] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Comment_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Order] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [ProductId] int NOT NULL,
+    CONSTRAINT [PK_Order] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Order_Product_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Product] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Order_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Cart_ProductId] ON [Cart] ([ProductId]);
+GO
+
+CREATE INDEX [IX_Cart_UserId] ON [Cart] ([UserId]);
+GO
+
+CREATE INDEX [IX_Comment_ProductId] ON [Comment] ([ProductId]);
+GO
+
+CREATE INDEX [IX_Comment_UserId] ON [Comment] ([UserId]);
+GO
+
+CREATE INDEX [IX_Order_ProductId] ON [Order] ([ProductId]);
+GO
+
+CREATE INDEX [IX_Order_UserId] ON [Order] ([UserId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240328194244_InitialCreate', N'8.0.3');
+GO
+
+COMMIT;
+GO
+
